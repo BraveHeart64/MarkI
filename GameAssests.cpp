04351c2a962d07a0b8aa8749f225a0776cf70dc7
Ryan <ryanlove64@gmail.com>
2023-01-25 21:47:50 -0600

    Update GameAssests.cpp

--- GameAssests.cpp
+++ GameAssests.cpp
@@ -1,6 +1,7 @@
 #include"GameAssets.h"
 
-
+// this is suppose to eventually load data onto the screen from a binary file. Each file it creates will be specific for the game its 
+// its creating. I am going to attempt to make a binary file out of GameAssests Objects.
 
 GameAssets::GameAssets(){
 
